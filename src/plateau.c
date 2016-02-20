8640a822ead691241e7c5414feb2e9a3ce8316e3
franck <franck.schmitt@hotmail.com>
2016-02-20 18:47:54 +0100

    Ajout des commentaires doxygen sur la gestion du plateau

--- src/plateau.c
+++ src/plateau.c
@@ -1,7 +1,24 @@
+/**
+ * \file plateau.c
+ * \author groupe Larakis
+ * \version 1
+ * \date 20/02/2016
+ * \brief Contrôle la gestion du plateau
+ * \details Toutes les fonctions necessaires à gérer le plateau : création, récupérer des valeurs, les modifier, afficher le plateau et libérer la mémoire
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "plateau.h"
 
+/**
+ * \fn initBoard
+ * \brief La fonction crée le plateau
+ * \details La fonction alloue la mémoire necessaire au plateau et remplis les éléments de la structure
+ * \param sizeX Entier qui done la taille en X du plateau créé
+ * \param sizeY Entier qui done la taille en Y du plateau créé
+ * \return Variable de type Board qui contiendra le plateau
+ */
 Board initBoard(int sizeX, int sizeY)
 {
   Board b;
@@ -26,6 +43,15 @@ Board initBoard(int sizeX, int sizeY)
   return b;
 }
 
+/**
+ * \fn getValue
+ * \brief La fonction renvoie la valeur d'un emplacement du plateau
+ * \details La fonction teste si les paramètres sont corrects et renvoie, le cas échéant, la valeur correspondante
+ * \param b Variable de type Board qui correspond au tableau à parcourir pour trouver la valeur
+ * \param posx Variable de type Int qui correpond à l'emplacement X de la valeur à récupérer
+ * \param posy Variable de type Int qui correpond à l'emplacement Y de la valeur à récupérer
+ * \return Variable de type int qui correspond à l'entier situé l'emplacement voulu sur le plateau
+ */
 int getValue(Board b, int posx, int posy)
 {
   // Check for out of range exceptions
@@ -40,6 +66,15 @@ int getValue(Board b, int posx, int posy)
   }
 }
 
+/**
+ * \fn setValue
+ * \brief La fonction attribue une valeur à un emplacement du plateau
+ * \details La fonction teste si les paramètres sont corrects et attribue, le cas échéant, la valeur correspondante au bon emplacement du plateau
+ * \param b Variable de type Board qui correspond au tableau à parcourir pour attribuer la valeur
+ * \param posx Variable de type Int qui correpond à l'emplacement X de la valeur à attriber
+ * \param posy Variable de type Int qui correpond à l'emplacement Y de la valeur à attribuer
+ * \param val Variable de type Int qui correpond à la valeur à attribuer
+ */
 void setValue(Board b, int posx, int posy, int val)
 {
   // Check for out of range exceptions
@@ -53,6 +88,12 @@ void setValue(Board b, int posx, int posy, int val)
   }
 }
 
+/**
+ * \fn displayBoard
+ * \brief La fonction affiche le plateau dans la console
+ * \details La fonction affiche pour chaque case du plateau sa valeur dans la console
+ * \param b Variable de type Board qui correspond au tableau à afficher
+ */
 void displayBoard(Board b)
 {
   int i, j;
@@ -66,6 +107,12 @@ void displayBoard(Board b)
   }
 }
 
+/**
+ * \fn freeBoard
+ * \brief La fonction libère la mémoire du plateau
+ * \details La fonction libère entièrement le tableau à deux dimensions de la mémoire
+ * \param b Variable de type Board qui correspond au tableau à effacer
+ */
 void freeBoard(Board b)
 {
   int i;
--- src/plateau.h
+++ src/plateau.h
@@ -1,3 +1,17 @@
+/**
+ * \file plateau.h
+ * \author groupe Larakis
+ * \version 1
+ * \date 20/02/2016
+ * \brief Entêtes des fonctions de la gestion du plateau
+ * \details Toutes les entêtes de fonctions necessaires à gérer le plateau : création, récupérer des valeurs, les modifier, afficher le plateau et libérer la mémoire
+ */
+
+/**
+ * \struct Board
+ * \brief La structure représente un plateau
+ * \details La structure contient un tableau à deux dimensions contenant les valeurs du plateau, un entier pour la taille X et un entier pour la taille Y
+ **/
 typedef struct Board
 {
   int** tab;
