6778c42b3900778b0d4fdbba2aefbf83f8a2369e
Evoluty <franck.schmitt@hotmail.com>
2016-03-01 12:16:30 +0100

    Ajout de fonctions pour permettre de déplacer le snake en console avec les touches directionnelles - Note : ajouter l'incrément automatique

--- src/game.c
+++ src/game.c
@@ -3,23 +3,32 @@
 #include <unistd.h>
 #include "game.h"
 
-static void checkMovement(Snake *s);
 
-Board* initGame()
+
+/*************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+#include <signal.h>
+#define SIZE 4
+/****************************/
+
+static void checkMovement(Snake *s);
+static void setBufferedInput(bool enable);
+
+void initGame()
 {
 	Board *b = boardInit(14, 14);
 	Snake *s1 = snakeCreate(3, 1);
 	Snake *s2 = snakeCreate(3, 2);
 	initSnakes(b, s1, s2);
 	boardDisplay(b);
-	while(1)
-	{
-		moveSnake(b, s1);
-		moveSnake(b, s2);
-		boardDisplay(b);
-		sleep(1);
-	}
-	return b;
+	playGame(b, s1, s2);
 }
 
 void initSnakes(Board *b, Snake *s1, Snake *s2)
@@ -36,7 +45,7 @@ void initSnakes(Board *b, Snake *s1, Snake *s2)
 	}
 	snakeSetDirection(s2, LEFT);
 	updateSnake(b, s1);
-	updateSnake(b, s2);
+	// updateSnake(b, s2);
 }
 
 void updateSnake(Board *b, Snake *s)
@@ -51,7 +60,6 @@ void updateSnake(Board *b, Snake *s)
 void moveSnake(Board *b, Snake *s)
 {
 	Way w = snakeGetWay(s);
-	snakeDisplay(s);
 	if (w == Normal)
 	{
 		boardSetValue(b, snakeGetPos(s, 0, Line), snakeGetPos(s, 0, Column), 0);
@@ -86,4 +94,70 @@ static void checkMovement(Snake *s)
 			printf("Error checkMovement\n");
 		break;
 	}	
+}
+
+
+
+
+static void setBufferedInput(bool enable) {
+	static bool enabled = true;
+	static struct termios old;
+	struct termios new;
+
+	if (enable && !enabled) {
+		// restore the former settings
+		tcsetattr(STDIN_FILENO,TCSANOW,&old);
+		// set the new state
+		enabled = true;
+	} else if (!enable && enabled) {
+		// get the terminal settings for standard input
+		tcgetattr(STDIN_FILENO,&new);
+		// we want to keep the old setting to restore them at the end
+		old = new;
+		// disable canonical mode (buffered i/o) and local echo
+		new.c_lflag &=(~ICANON & ~ECHO);
+		// set the new settings immediately
+		tcsetattr(STDIN_FILENO,TCSANOW,&new);
+		// set the new state
+		enabled = false;
+	}
+}
+
+void playGame(Board *b, Snake *s1, Snake *s2)
+{
+	setBufferedInput(false);
+	char c;
+	bool touche = false;
+	while (true) 
+	{
+		c = getchar();
+		switch(c) 
+		{
+			case 68:	// left arrow
+				snakeSetDirection(s1, LEFT); 
+				touche = true; 
+			break;
+			case 67:	// right arrow
+				snakeSetDirection(s1, RIGHT);
+				touche = true;  
+			break;
+			case 66:	// up arrow
+				snakeSetDirection(s1, UP);   
+				touche = true;
+			break;
+			case 65:	// down arrow
+				snakeSetDirection(s1, DOWN); 
+				touche = true;  
+			break;
+			default: 
+				touche = false;
+			break;
+		}
+		if (touche)
+		{			
+			moveSnake(b, s1);
+			boardDisplay(b);
+		}
+	}
+	setBufferedInput(true);
 }
--- src/game.h
+++ src/game.h
@@ -4,9 +4,10 @@
 #include "board.h"
 #include "snake.h"
 
-Board* initGame();
+void initGame();
 void initSnakes(Board *b, Snake *s1, Snake *s2);
 void updateSnake(Board *b, Snake *s);
 void moveSnake(Board *b, Snake *s);
+void playGame(Board *b, Snake *s1, Snake *s2);
 
 #endif
