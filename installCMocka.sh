2ce683a01d118bf89348fd7ff5c02b47a48daeb9
saetlan <s.arnaud67@hotmail.fr>
2016-03-20 20:42:10 +0100

    creation of an installation script for cmocka
    Launch it to install cmocka

---
+++ installCMocka.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install used package
+sudo apt-get install wget
+sudo apt-get install cmake
+
+#install cmocka
+wget https://open.cryptomilk.org/attachments/download/54/cmocka-1.0.0.tar.xz
+xz -d cmocka-1.0.0.tar.xz
+tar vxf cmocka-1.0.0.tar
+cd cmocka-1.0.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
