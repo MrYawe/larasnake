56f0a2008569ac693a94aff5155eacb3c20e7f40
saetlan <s.arnaud67@hotmail.fr>
2016-03-21 14:07:09 +0100

    script udpate installation cmocka

--- installCMocka.sh
+++ installCMocka.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 #install used package
-sudo apt-get install wget
-sudo apt-get install cmake
+sudo apt-get install cmake -y --force-yes
 
 #install cmocka
-wget https://open.cryptomilk.org/attachments/download/54/cmocka-1.0.0.tar.xz
-xz -d cmocka-1.0.0.tar.xz
-tar vxf cmocka-1.0.0.tar
-cd cmocka-1.0.0
+cd lib/cmocka-1.0.0
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo make install
-rm cmocka-1.0.0.tar
-rm cmocka-1.0.0
+cd ../../..
