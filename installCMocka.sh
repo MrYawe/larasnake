29244ae22239ba320c1d99f9fa52043c4b063f68
saetlan <s.arnaud67@hotmail.fr>
2016-03-21 15:09:07 +0100

    Final installation cmocka

--- installCMocka.sh
+++ installCMocka.sh
@@ -2,9 +2,20 @@
 
 #install used package
 sudo apt-get install cmake -y --force-yes
+sudo apt-get install wget -y --force-yes
 
 #install cmocka
-cd lib/cmocka-1.0.0/build
+mkdir lib
+
+wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
+xz -d cmocka-1.0.1.tar.xz
+tar vxf cmocka-1.0.1.tar
+rm cmocka-1.0.1.tar
+mv cmocka-1.0.1 lib/
+
+cd lib/cmocka-1.0.1
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo make install
